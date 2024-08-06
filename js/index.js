91e0b6cea355b658d6c5682e1200ad7460b98ef0
jacquelinekharouf <163605355+jacquelinekharouf@users.noreply.github.com>
2024-08-06 13:59:25 -0600

    8.6.24
    working on using jQuery for UI element (toggle for My Skills items)

--- js/index.js
+++ js/index.js
@@ -1,35 +1,34 @@
 
 console.log("Your index.js file is loaded correctly!");
+$("#a").css("display","none");
+$("h3").css("display","none");
 
-$(".mySkillsBox").on("click", function(){
+
+$("#child1button").on("click", function(){
     $(document).ready(function(){
-        
-          $("#images/uc-writing-skills.png").toggle();
+          $("#a:contains(I empathize with the user’s problem and write a solution that fits the user’s needs and goals.)").toggle();
         });
-      });
-    
-
-
-$(".mySkillsBox").on("click", function(){
-$(document).ready(function(){
-    $("h6").click(function(){
-      $(this).show();
-    });
-});
+    $(document).ready(function(){
+            $("h3:contains(User-Centered Writing)").toggle();
+          });
 });
 
-$(".myToolsBox").on("click", function(){
-    console.log("You clicked on .myToolsBox");
+$("#child2button").on("click", function(){
+    $(document).ready(function(){
+          $("#a:contains(I craft surveys that collect accurate data and help my team develop a full understanding of the typical user population.)").toggle();
+        });
+    $(document).ready(function(){
+            $("h3:contains(UX Research)").toggle();
+          });
 });
 
-$(document).ready(function(){
-    $(".child1").click(function(){
-      $(".child1").slideDown("slow");
-    });
-  });
+$("#child3button").on("click", function(){
+    $(document).ready(function(){
+          $("#a:contains(I write questions that get to the heart of the problem. I listen to the user and take notes so that I can ask better follow-up questions.)").toggle();
+        });
+    $(document).ready(function(){
+            $("h3:contains(Qualitative Interviews)").toggle();
+          });
+});
 
-  $(document).ready(function(){
-    $("child1").click(function(){
-      $("h3").toggle();
-    });
-  });
+
