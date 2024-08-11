7f5e48209023b0125ea3d0d574bb281680a09904
jacquelinekharouf <163605355+jacquelinekharouf@users.noreply.github.com>
2024-08-11 15:16:48 -0600

    8.11.24
    iterated changes based on feedback

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 
 console.log("Your index.js file is loaded correctly!");
+$("p1#b").css("display","none");
 $("h6#a").css("display","none");
 $("#a").css("display","none");
 $("h3#x").css("display","none")
@@ -13,7 +14,7 @@ $("#child1button").mouseenter(function(){
 
 $("#child1button").on("click", function(){
     $(document).ready(function(){
-          $("h6#a:contains(I empathize with the user’s problem and write a solution that fits the user’s needs and goals.)").toggle();
+          $("p1#b:contains(I empathize with the user’s problem and write a solution that fits the user’s needs and goals.)").toggle();
         });
     $(document).ready(function(){
             $("h3#x:contains(User-Centered Writing)").toggle();
@@ -28,7 +29,7 @@ $("#child2button").mouseenter(function(){
 });
 $("#child2button").on("click", function(){
     $(document).ready(function(){
-          $("h6:contains(I craft surveys that collect accurate data and help my team develop a full understanding of the typical user population.)").toggle();
+          $("p1:contains(I craft surveys that collect accurate data and help my team develop a full understanding of the typical user population.)").toggle();
         });
     $(document).ready(function(){
             $("h3:contains(UX Research)").toggle();
@@ -43,7 +44,7 @@ $("#child3button").mouseenter(function(){
 });
 $("#child3button").on("click", function(){
     $(document).ready(function(){
-          $("h6:contains(I write questions that get to the heart of the problem. I listen to the user and take notes so that I can ask better follow-up questions.)").toggle();
+          $("p1:contains(I write questions that get to the heart of the problem. I listen to the user and take notes so that I can ask better follow-up questions.)").toggle();
         });
     $(document).ready(function(){
             $("h3:contains(Qualitative Interviews)").toggle();
@@ -59,7 +60,7 @@ $("#child1buttonT").mouseenter(function(){
 
 $("#child1buttonT").on("click", function(){
     $(document).ready(function(){
-          $("h6:contains(My favorite and most basic tool. By using paper and pencil, I visualize my ideas and leave room for error.)").toggle();
+          $("p1:contains(My favorite and most basic tool. By using paper and pencil, I visualize my ideas and leave room for error.)").toggle();
         });
     $(document).ready(function(){
             $("h3:contains(Paper & Pencil)").toggle();
@@ -75,7 +76,7 @@ $("#child2buttonT").mouseenter(function(){
 
 $("#child2buttonT").on("click", function(){
     $(document).ready(function(){
-          $("h6:contains(The second-best tool available to a UX designer. Figma allows me to bring my sketches to life.)").toggle();
+          $("p1:contains(The second-best tool available to a UX designer. Figma allows me to bring my sketches to life.)").toggle();
         });
     $(document).ready(function(){
             $("h3:contains(Figma)").toggle();
@@ -91,7 +92,7 @@ $("#child3buttonT").mouseenter(function(){
 
 $("#child3buttonT").on("click", function(){
     $(document).ready(function(){
-          $("h6:contains(I live to stay organized and Google Drive is simply one of the best tools to do just that.)").toggle();
+          $("p1:contains(I live to stay organized and Google Drive is simply one of the best tools to do just that.)").toggle();
         });
     $(document).ready(function(){
             $("h3:contains(Google Drive)").toggle();
